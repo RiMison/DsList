4dc0c298a4f7e3f10a9a322924b06fcad534c8e9
Ricardo Mison <118085266+RiMison@users.noreply.github.com>
2025-05-25 15:53:06 -0300

    Aula3

--- DsList/src/main/java/com/devsuperior/DsList/controllers/GameListController.java
+++ DsList/src/main/java/com/devsuperior/DsList/controllers/GameListController.java
@@ -5,11 +5,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.DsList.dto.GameListDTO;
 import com.devsuperior.DsList.dto.GameMinDTO;
+import com.devsuperior.DsList.dto.ReplacementDTO;
 import com.devsuperior.DsList.services.GameListService;
 import com.devsuperior.DsList.services.GameService;
 
@@ -37,6 +40,12 @@ public class GameListController {
 		
 	}
 	
+	@PostMapping(value = "/{listId}/replacement")
+	public void move(@PathVariable Long listId, @RequestBody ReplacementDTO body){
+		gameListService.move(listId, body.getSourceIndex(), body.getDestinationIndex());
+		
+		
+	}
 	
 	
 	
